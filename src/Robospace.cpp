0565783c1a1f60fe6858f54d9f93ec17c5d1e3e0
Alexey Shmalko <rasen.dubi@gmail.com>
2014-03-27 23:36:26 +0200

    Add a robot which is capable to move
    Robot is controlled by arrow keys. Code is ugly and
    should be refactored. But at least, it works.
    
    Robot should be factored out into a separate class.

--- src/Robospace.cpp
+++ src/Robospace.cpp
@@ -2,6 +2,11 @@
 
 #include <Polycode.h>
 #include <PolycodeView.h>
+#include <PolyEvent.h>
+#include <PolyScreenShape.h>
+#include <PolyVector2.h>
+
+#include <cmath>
 
 Robospace::Robospace() {
 	view = new Polycode::PolycodeView("Robospace");
@@ -14,8 +19,73 @@ Robospace::Robospace() {
 	Polycode::Screen *screen = new Polycode::Screen();
 	Polycode::ScreenLabel *label = new Polycode::ScreenLabel("Hello, Robospace!", 32);
 	screen->addChild(label);
+
+	robot = new Polycode::ScreenShape(Polycode::ScreenShape::SHAPE_RECT, 64, 64);
+	robot->setPositionMode(Polycode::ScreenEntity::POSITION_CENTER);
+	robot->setPosition(320, 240);
+	screen->addChild(robot);
+	rotationSpeed = 0;
+	forwardSpeed = 0;
+
+	core->getInput()->addEventListener(this, Polycode::InputEvent::EVENT_KEYDOWN);
+	core->getInput()->addEventListener(this, Polycode::InputEvent::EVENT_KEYUP);
+}
+
+void Robospace::handleEvent(Polycode::Event *e) {
+	using Polycode::InputEvent;
+
+	if (e->getDispatcher() == core->getInput()) {
+		InputEvent *event = static_cast<InputEvent *>(e);
+
+		switch (e->getEventCode()) {
+			case InputEvent::EVENT_KEYDOWN:
+				switch (event->keyCode()) {
+					case Polycode::KEY_UP:
+						forwardSpeed = 100;
+						break;
+
+					case Polycode::KEY_DOWN:
+						forwardSpeed = -100;
+						break;
+
+					case Polycode::KEY_LEFT:
+						rotationSpeed = -100;
+						break;
+
+					case Polycode::KEY_RIGHT:
+						rotationSpeed = 100;
+						break;
+				}
+				break;
+			case InputEvent::EVENT_KEYUP:
+				switch (event->keyCode()) {
+					case Polycode::KEY_LEFT:
+					case Polycode::KEY_RIGHT:
+						rotationSpeed = 0;
+						break;
+
+					case Polycode::KEY_UP:
+					case Polycode::KEY_DOWN:
+						forwardSpeed = 0;
+						break;
+				}
+				break;
+		}
+	}
 }
 
 void Robospace::run() {
-	while (core->updateAndRender());
+	do {
+		Number elapsed = core->getElapsed();
+
+		Polycode::Vector2 pos = robot->getPosition2D();
+		pos.x += elapsed * forwardSpeed * std::cos(robot->getRotation() / 180 * M_PI);
+		pos.y += elapsed * forwardSpeed * std::sin(robot->getRotation() / 180 * M_PI);
+
+		robot->setPosition(pos);
+		if (forwardSpeed) {
+			robot->setRotation(robot->getRotation() +
+					elapsed * rotationSpeed * (forwardSpeed > 0 ? 1 : -1));
+		}
+	} while (core->updateAndRender());
 }
--- src/Robospace.h
+++ src/Robospace.h
@@ -1,23 +1,34 @@
 #ifndef ROBOSPACE_H_
 #define ROBOSPACE_H_
 
+#include <PolyEventHandler.h>
+
 namespace Polycode {
 	class Core;
+	class Event;
 	class PolycodeView;
+	class ScreenShape;
 }
 
 /**
  * The main game class.
  */
-class Robospace {
+class Robospace : public Polycode::EventHandler {
 public:
 	explicit Robospace();
 
 	void run();
 
+	void handleEvent(Polycode::Event *e);
+
 private:
 	Polycode::Core *core;
 	Polycode::PolycodeView *view;
+
+	Polycode::ScreenShape *robot;
+
+	double rotationSpeed;
+	double forwardSpeed;
 
 };
 
